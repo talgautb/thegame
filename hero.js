3136946ac33ac3cce45360d56aeb4418148748c2
Talat Uspanov <talgat.uspanov@bee.kz>
2016-04-16 15:11:34 +0600

    add hero

---
+++ hero.js
@@ -0,0 +1,45 @@
+/**
+ * Hero.js - 16/04/2016
+ */
+var bullets;
+var fireRate = 100;
+var nextFire = 0;
+
+// Initialization our hero
+function createHero() {
+  bullets = game.add.group();
+  bullets.enableBody = true;
+  bullets.physicsBodyType = Phaser.Physics.ARCADE;
+
+  bullets.createMultiple(50, 'bullet');
+  bullets.setAll('checkWorldBounds', true);
+  bullets.setAll('outOfBoundsKill', true);
+  
+  sprite = game.add.sprite(400, 300, 'hero');
+  sprite.anchor.set(0.5);
+}
+
+function updateHero() {
+  sprite.rotation = game.physics.arcade.angleToPointer(sprite);
+
+  if (game.input.activePointer.isDown) {
+    fire();
+  }
+}
+
+function fire() {
+  if (game.time.now > nextFire && bullets.countDead() > 0) {
+    nextFire = game.time.now + fireRate;
+
+    var bullet = bullets.getFirstDead();
+
+    bullet.reset(sprite.x - 8, sprite.y - 8);
+
+    game.physics.arcade.moveToPointer(bullet, 300);
+  }
+}
+
+function renderHero() {
+  game.debug.text('Active Bullets: ' + bullets.countLiving() + ' / ' + bullets.total, 32, 32);
+  game.debug.spriteInfo(sprite, 32, 450);
+}
--- index.js
+++ index.js
@@ -3,52 +3,40 @@
  */
 
  var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+var sprite;
 
 function preload() {
+  //  You can fill the preloader with as many assets as your game requires
 
-    //  You can fill the preloader with as many assets as your game requires
+  //  Here we are loading an image. The first parameter is the unique
+  //  string by which we'll identify the image later in our code.
 
-    //  Here we are loading an image. The first parameter is the unique
-    //  string by which we'll identify the image later in our code.
-
-    //  The second parameter is the URL of the image (relative)
-    game.load.image('phaser', 'assets/sprites/phaser.png');
+  //  The second parameter is the URL of the image (relative)
+  // game.load.image('arrow', 'assets/sprites/arrow.png');
+  game.load.image('hero', 'assets/hero/hero.png');
+  game.load.image('bullet', 'assets/hero/tomato.png');
 
 }
 
-var sprite;
-
 function create() {
+  //  To make the sprite move we need to enable Arcade Physics
+  game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    //  To make the sprite move we need to enable Arcade Physics
-    game.physics.startSystem(Phaser.Physics.ARCADE);
+  sprite = game.add.sprite(game.world.centerX, game.world.centerY, 'phaser');
+  sprite.anchor.set(0.5);
 
-    sprite = game.add.sprite(game.world.centerX, game.world.centerY, 'phaser');
-    sprite.anchor.set(0.5);
+  // Create our hero here
+  createHero();
 
-    //  And enable the Sprite to have a physics body:
-    game.physics.arcade.enable(sprite);
+  //  And enable the Sprite to have a physics body:
+  game.physics.arcade.enable(sprite, Phaser.Physics.ARCADE);
 
 }
 
 function update () {
-
-    //  If the sprite is > 8px away from the pointer then let's move to it
-    if (game.physics.arcade.distanceToPointer(sprite, game.input.activePointer) > 8)
-    {
-        //  Make the object seek to the active pointer (mouse or touch).
-        game.physics.arcade.moveToPointer(sprite, 300);
-    }
-    else
-    {
-        //  Otherwise turn off velocity because we're close enough to the pointer
-        sprite.body.velocity.set(0);
-    }
-
+  updateHero();
 }
 
 function render () {
-
-	game.debug.inputInfo(32, 32);
-
+  renderHero();
 }
