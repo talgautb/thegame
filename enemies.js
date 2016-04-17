7599b9619fe31bb6e8142e7041372589b19254f7
Talat Uspanov <talgat.uspanov@bee.kz>
2016-04-17 17:24:29 +0600

    kill and create enemy

--- enemies.js
+++ enemies.js
@@ -1,29 +1,22 @@
 function createEnemy(type) {
-	if (type == 3) {
-
-	}
+  var lvl = (type) ? type : 3;
+  if (lvl === 3) {
+    var enemy = game.add.sprite(game.world.randomX, game.world.randomY, 'enemyTriangle');
+    game.physics.arcade.enable(enemy, Phaser.Physics.ARCADE);
+    game.add.tween(enemy.body).to( center, 10000, Phaser.Easing.Linear.None, true);
+    enemies.push(enemy);
+  }
 }
 
 
 function generateEnemies(level) {
+  var lvl = (level) ? level : 3;
 
-
-
-	for (var i = 0; i < 10; i++)
-    {
-        var enemy = game.add.sprite(game.world.randomX, game.world.randomY, 'enemyTriangle');
-        game.physics.arcade.enable(enemy, Phaser.Physics.ARCADE);
-        game.add.tween(enemy.body).to( center, 10000, Phaser.Easing.Linear.None, true);
-
-
-        enemies.push(enemy);
-
-
-        
-    }
+  for (var i = 0; i < 10; i++) {
+    createEnemy(lvl);
+  }
 }
 
 function moveEnemies() {
-
-	
+  // go go
 }
--- index.js
+++ index.js
@@ -2,10 +2,16 @@
  * The game - index.js
  */
 
-var game = new Phaser.Game(1500, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+var game = new Phaser.Game(1500, 600, Phaser.AUTO, 'phaser-example',
+  {
+    preload: preload,
+    create: create,
+    update: update,
+    render: render
+  });
 var sprite;
 var enemyTriangle;
-var currentLevel = 1;
+var baseLevel = 3; // 3 == triangle
 var enemies = [];
 
 var center = {
@@ -15,36 +21,32 @@ var center = {
 
 function preload() {
   //  You can fill the preloader with as many assets as your game requires
-
 	game.load.image('enemyTriangle', 'assets/enemies/triangle.png');    
 	game.load.image('hero', 'assets/hero/hero.png');
-  	game.load.image('bullet', 'assets/hero/tomato.png');
+  game.load.image('bullet', 'assets/hero/tomato.png');
 }
 
-
 function create() {
-    //  To make the sprite move we need to enable Arcade Physics
-    game.physics.startSystem(Phaser.Physics.ARCADE);
-
-  
-	generateEnemies(currentLevel);
-    
-
-  	// Create our hero here
-  	createHero();
-
-  	//  And enable the Sprite to have a physics body:
-  	game.physics.arcade.enable(sprite, Phaser.Physics.ARCADE);
-
-  	
-    
+  //  To make the sprite move we need to enable Arcade Physics
+  game.physics.startSystem(Phaser.Physics.ARCADE);
+	// generate 10 enemies
+  generateEnemies(baseLevel);
+	// Create our hero here
+	createHero();
+	//  And enable the Sprite to have a physics body:
+	game.physics.arcade.enable(sprite, Phaser.Physics.ARCADE);
  }
 
 function update () {
+  moveEnemies();
+	updateHero();
+  game.physics.arcade.collide(bullets, enemies, killEnemy, null, this);
+}
 
-    moveEnemies();
-  	updateHero();
-
+function killEnemy(enemy, bullet) {
+  enemy.kill();
+  bullet.kill();
+  createEnemy(baseLevel);
 }
 
 function render () {
